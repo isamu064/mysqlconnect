2ae732e2370bb41613bf26dc045e6609c3b71b17
sysek <sysek@outlook.com>
2014-12-05 15:36:49 +0000

    First commit

---
+++ MysqlConnect/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace MysqlConnect
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string pass = "";
+			string host = "";
+			string username = "";
+			string database = "";
+
+			ConsoleKeyInfo key;
+			Console.Write ("Host: ");
+			host = Console.ReadLine ();
+			Console.Write ("User: ");
+			username = Console.ReadLine ();
+			Console.Write ("Database: ");
+			database = Console.ReadLine ();
+
+			System.Console.Write("Podaj hasło do bazy danych: ");
+			do
+			{
+				key = Console.ReadKey(true);
+				if (key.Key != ConsoleKey.Backspace)
+				{
+					pass += key.KeyChar;
+					Console.Write("*");
+				}
+				else
+				{
+					if (key.Key == ConsoleKey.Backspace && pass.Length > 0)
+					{
+						pass = pass.Substring(0, (pass.Length - 1));
+						Console.Write("\b\b");
+					}
+				}
+			} while (key.Key != ConsoleKey.Enter);
+
+			// string connStr = "server=;user=;database=;password="
+			string connStr = "server=" + host + ";user=" + username + ";database=" + database + ";password=" + pass;
+			MySqlConnection conn = new MySqlConnection(connStr);
+
+			try
+			{
+				Console.WriteLine("\nConnecting to MySQL");
+				conn.Open();
+			}
+			catch(Exception ex) {
+				Console.Write (ex.ToString ());
+				conn.Close();
+				Environment.Exit (0);
+			}
+
+			string command;
+			Console.WriteLine ("Połączono do bazy {0}", database);
+			while (key.Key != ConsoleKey.Q) {
+				Console.Write ("> ");
+				command = Console.ReadLine ();
+			}
+
+		}
+	}
+}
